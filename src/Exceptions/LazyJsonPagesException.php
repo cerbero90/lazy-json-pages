fd684f29522b6faf3d6188bcb17351c2f8b35c3c
Andrea Marco Sartori <andrea.marco.sartori@gmail.com>
2021-07-27 23:14:42 +1000

    Create exception

---
+++ src/Exceptions/LazyJsonPagesException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Cerbero\LazyJsonPages\Exceptions;
+
+use Exception;
+
+/**
+ * The lazy JSON pages exception.
+ *
+ */
+class LazyJsonPagesException extends Exception
+{
+    //
+}
