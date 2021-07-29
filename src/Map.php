a9fc1c2b3e6058d8f907826fecf232865b67003d
Andrea Marco Sartori <andrea.marco.sartori@gmail.com>
2021-07-29 20:50:43 +1000

    Update map

--- src/Map.php
+++ src/Map.php
@@ -6,7 +6,7 @@ use Cerbero\LazyJson\Concerns\EndpointAware;
 use Illuminate\Http\Client\Response;
 
 /**
- * The JSON map.
+ * The JSON API map.
  *
  */
 class Map
@@ -35,6 +35,13 @@ class Map
     public $pageName = 'page';
 
     /**
+     * The number of the first page.
+     *
+     * @var int
+     */
+    public $firstPage = 1;
+
+    /**
      * The total number of pages.
      *
      * @var int
@@ -49,13 +56,6 @@ class Map
     public $items;
 
     /**
-     * The query parameter holding the number of items per page.
-     *
-     * @var string
-     */
-    public $perPageQuery;
-
-    /**
      * The number of items per page.
      *
      * @var int
@@ -63,41 +63,90 @@ class Map
     public $perPage;
 
     /**
-     * The next page(s) of a simple or cursor pagination.
+     * The query parameter holding the number of items per page.
      *
-     * @var array|string|int
+     * @var string
+     */
+    public $perPageQuery;
+
+    /**
+     * The new number of items per page.
+     *
+     * @var int
+     */
+    public $perPageOverride;
+
+    /**
+     * The next page of a simple or cursor pagination.
+     *
+     * @var string|int
      */
     public $nextPage;
+
+    /**
+     * The key holding the next page.
+     *
+     * @var string
+     */
+    public $nextPageKey;
+
+    /**
+     * The number of the last page.
+     *
+     * @var int
+     */
+    public $lastPage;
+
+    /**
+     * The timeout in seconds.
+     *
+     * @var int
+     */
+    public $timeout = 5;
+
+    /**
+     * The number of attempts to fetch pages.
+     *
+     * @var int
+     */
+    public $attempts = 3;
+
+    /**
+     * The backoff strategy.
+     *
+     * @var callable
+     */
+    public $backoff;
 
     /**
      * Instantiate the class.
      *
      * @param Response $source
      * @param string $path
-     * @param callable|array|string|int $data
+     * @param callable|array|string|int $map
      */
-    public function __construct(Response $source, string $path, $data)
+    public function __construct(Response $source, string $path, $map)
     {
         $this->source = $source;
         $this->path = $path;
-        $this->hydrateMap($data);
+        $this->hydrateMap($map);
     }
 
     /**
-     * Hydrate the map with the given data
+     * Hydrate the map
      *
-     * @param callable|array|string|int $data
+     * @param callable|array|string|int $map
      * @return void
      */
-    protected function hydrateMap($data): void
+    protected function hydrateMap($map): void
     {
-        if (is_callable($data)) {
-            $data($this);
-        } elseif (is_array($data)) {
-            [$this->pageName => $value] = $data;
-            $this->pages = $this->resolveInt($value);
+        if (is_callable($map)) {
+            $map($this);
+        } elseif (is_array($map)) {
+            [$this->pageName => $value] = $map;
+            $this->pages = $this->resolveInt($value, 1);
         } else {
-            $this->pages = $this->resolveInt($data);
+            $this->pages = $this->resolveInt($map, 1);
         }
     }
 
@@ -105,22 +154,38 @@ class Map
      * Retrieve an integer from the given value
      *
      * @param string|int $value
+     * @param int $minimum
      * @return int
      */
-    protected function resolveInt($value): int
+    protected function resolveInt($value, int $minimum): int
     {
-        return intval(is_numeric($value) ? $value : $this->source->json($value));
+        $raw = is_numeric($value) ? $value : $this->source->json($value);
+
+        return (int) max($raw, $minimum);
     }
 
     /**
      * Set the page name
      *
      * @param string $name
-     * @return static
+     * @return self
      */
-    public function pageName(string $name): static
+    public function pageName(string $name): self
     {
         $this->pageName = $name;
+
+        return $this;
+    }
+
+    /**
+     * Set the number of the first page
+     *
+     * @param int $page
+     * @return self
+     */
+    public function firstPage(int $page): self
+    {
+        $this->firstPage = max(0, $page);
 
         return $this;
     }
@@ -129,11 +194,11 @@ class Map
      * Set the total number of pages
      *
      * @param string|int $pages
-     * @return static
+     * @return self
      */
-    public function pages($pages): static
+    public function pages($pages): self
     {
-        $this->pages = $this->resolveInt($pages);
+        $this->pages = $this->resolveInt($pages, 1);
 
         return $this;
     }
@@ -142,26 +207,28 @@ class Map
      * Set the total number of items
      *
      * @param string|int $items
-     * @return static
+     * @return self
      */
-    public function items($items): static
+    public function items($items): self
     {
-        $this->items = $this->resolveInt($items);
+        $this->items = $this->resolveInt($items, 0);
 
         return $this;
     }
 
     /**
-     * Set the number of items per page and its query parameter
+     * Set the number of items per page and optionally override it
      *
-     * @param string $query
      * @param int $perPage
-     * @return static
+     * @param string|null $query
+     * @param int $firstPageItems
+     * @return self
      */
-    public function perPage(string $query, int $perPage): static
+    public function perPage(int $perPage, string $query = null, int $firstPageItems = 1): self
     {
+        $this->perPage = $query ? $firstPageItems : $perPage;
         $this->perPageQuery = $query;
-        $this->perPage = $perPage;
+        $this->perPageOverride = $query ? $perPage : null;
 
         return $this;
     }
@@ -170,25 +237,82 @@ class Map
      * Set the next page
      *
      * @param string $key
-     * @return static
+     * @return self
      */
-    public function nextPage(string $key): static
+    public function nextPage(string $key): self
     {
-        $nextPages = [];
-        $page = $this->source->json($key);
-        $hasManyPages = is_array($page);
-        $isEndpoint = $this->isEndpoint($hasManyPages ? head($page) : $page);
+        $this->nextPageKey = $key;
+        $this->nextPage = $this->resolvePage($this->source->json($key));
 
-        foreach ((array) $page as $value) {
-            if ($isEndpoint) {
-                parse_str(parse_url($value, PHP_URL_QUERY), $query);
-                $value = $query[$this->pageName];
-            }
+        return $this;
+    }
 
-            $nextPages[] = is_numeric($value) ? intval($value) : $value;
+    /**
+     * Retrieve the page value from the given presumed URL
+     *
+     * @param mixed $value
+     * @return mixed
+     */
+    protected function resolvePage($value)
+    {
+        if ($this->isEndpoint($value)) {
+            parse_str(parse_url($value, PHP_URL_QUERY), $query);
+            $value = $query[$this->pageName];
         }
 
-        $this->nextPage = $hasManyPages ? $nextPages : head($nextPages);
+        return is_numeric($value) ? intval($value) : $value;
+    }
+
+    /**
+     * Set the number of the last page
+     *
+     * @param string|int $page
+     * @return self
+     */
+    public function lastPage($page): self
+    {
+        $raw = is_numeric($page) ? $page : $this->source->json($page);
+
+        $this->lastPage = $this->resolvePage($raw);
+
+        return $this;
+    }
+
+    /**
+     * Set the timeout in seconds
+     *
+     * @param int $seconds
+     * @return self
+     */
+    public function timeout(int $seconds): self
+    {
+        $this->timeout = $seconds;
+
+        return $this;
+    }
+
+    /**
+     * Set the number of attempts to fetch pages
+     *
+     * @param int $times
+     * @return self
+     */
+    public function attempts(int $times): self
+    {
+        $this->attempts = $times;
+
+        return $this;
+    }
+
+    /**
+     * Set the backoff strategy
+     *
+     * @param callable $callback
+     * @return self
+     */
+    public function backoff(callable $callback): self
+    {
+        $this->backoff = $callback;
 
         return $this;
     }
