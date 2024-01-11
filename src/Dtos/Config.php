1527db487ab404f615ca29bf1b6bd069cc4a6222
Andrea Marco Sartori <andrea.marco.sartori@gmail.com>
2024-01-11 19:32:22 +1000

    Update configuration

--- src/Dtos/Config.php
+++ src/Dtos/Config.php
@@ -10,19 +10,21 @@ final class Config
 {
     public function __construct(
         public readonly string $dot,
+        public readonly string $pointer,
         public readonly string $pageName,
         public readonly int $firstPage,
         public readonly ?int $totalPages,
         public readonly ?int $totalItems,
         public readonly ?int $perPage,
         public readonly ?string $perPageKey,
-        public readonly int $perPageOverride,
+        public readonly ?int $perPageOverride,
         public readonly ?Closure $nextPage,
-        public readonly string $nextPageKey,
-        public readonly int $lastPage,
-        public readonly int $chunk,
-        public readonly int $concurrency,
-        public readonly int $timeout,
+        public readonly ?string $nextPageKey,
+        public readonly ?int $lastPage,
+        public readonly int $async,
+        public readonly int $connectionTimeout,
+        public readonly int $requestTimeout,
         public readonly int $attempts,
+        public readonly ?Closure $backoff,
     ) {}
 }
