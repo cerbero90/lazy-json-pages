d53537dfae9beb0a1258db7b1a86afc537fef317
Andrea Marco Sartori <andrea.marco.sartori@gmail.com>
2024-01-20 20:44:33 +1000

    Create singleton client

---
+++ src/Services/Client.php
@@ -0,0 +1,72 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Cerbero\LazyJsonPages\Services;
+
+use GuzzleHttp\Client as Guzzle;
+use GuzzleHttp\RequestOptions;
+
+/**
+ * The client singleton.
+ */
+final class Client
+{
+    /**
+     * The default options.
+     *
+     * @var array<string, mixed>
+     */
+    private static array $defaultOptions = [
+        RequestOptions::STREAM => true,
+        RequestOptions::HEADERS => [
+            'Accept' => 'application/json',
+            'Content-Type' => 'application/json',
+        ],
+    ];
+
+    /**
+     * The custom options.
+     */
+    private static array $options = [];
+
+    /**
+     * The Guzzle client instance.
+     */
+    private static ?Guzzle $guzzle = null;
+
+    /**
+     * Instantiate the class.
+     */
+    private function __construct()
+    {
+        // disable the constructor
+    }
+
+    /**
+     * Set the Guzzle client options.
+     */
+    public static function configure(array $options): void
+    {
+        self::$options = array_replace_recursive(self::$options, $options);
+    }
+
+    /**
+     * Retrieve the Guzzle client instance.
+     */
+    public static function instance(): Guzzle
+    {
+        return self::$guzzle ??= new Guzzle(
+            array_replace_recursive(self::$defaultOptions, self::$options),
+        );
+    }
+
+    /**
+     * Clean up the static values.
+     */
+    public static function reset(): void
+    {
+        self::$guzzle = null;
+        self::$options = [];
+    }
+}
