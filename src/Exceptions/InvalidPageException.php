e5ced02d35f1e92ff93c4b75b669bddd9483d710
Andrea Marco Sartori <andrea.marco.sartori@gmail.com>
2024-01-22 19:34:53 +1000

    Create exception

--- src/Exceptions/InvalidPageException.php
+++ src/Exceptions/InvalidPageException.php
@@ -2,10 +2,6 @@
 
 namespace Cerbero\LazyJsonPages\Exceptions;
 
-use Cerbero\LazyJsonPages\Services\Outcome;
-use GuzzleHttp\Exception\TransferException;
-use Illuminate\Support\LazyCollection;
-
 /**
  * The exception thrown when a given JSON key does not contain a valid page.
  */
